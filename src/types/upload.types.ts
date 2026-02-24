4b00125a1822b7878fd59c3ee25f2ae9dcd948b4
Saurav Kumar <saurav.sde.cs@gmail.com>
2026-02-25 01:40:59 +0530

    made upload types for input validation

--- src/types/upload.types.ts
+++ src/types/upload.types.ts
@@ -1,5 +1,6 @@
 
 import {z} from 'zod'
-import type { getUploadUrlSchema } from '../schemas/upload.schema.js'
+import type { getFetchUrlSchema, getUploadUrlSchema } from '../schemas/upload.schema.js'
 
 export type getUploadUrlInput = z.infer<typeof getUploadUrlSchema>['body'];
+export type getFetchUrlInput = z.infer<typeof getFetchUrlSchema>['body'];
