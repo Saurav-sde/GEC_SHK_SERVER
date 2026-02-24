f3405d629eeb04d743d11c633a1b84a691a0b270
Saurav Kumar <saurav.sde.cs@gmail.com>
2026-02-25 01:41:35 +0530

    updated routes

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import cookieParser from 'cookie-parser';
 import express from 'express';
 import authRouter from './routes/auth.router.js';
 import { globalErrorHandler } from './middlewares/error.middleware.js';
+import uploadRouter from './routes/upload.router.js';
 
 const app = express();
 // import cors from 'cors';
@@ -12,6 +13,7 @@ app.use(cookieParser());
 
 // routes
 app.use('/api/auth', authRouter);
+app.use('/api/upload', uploadRouter)
 
 
 
