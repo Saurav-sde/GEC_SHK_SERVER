97d61dbc27b8cb262972426676cc204bf27c41c1
Saurav Kumar <saurav.sde.cs@gmail.com>
2026-03-11 21:38:25 +0530

    created routes for register admin and faculty

--- src/routes/auth.router.ts
+++ src/routes/auth.router.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import { validateMiddleware } from '../middlewares/validateMiddleware.js';
-import { studentRegisterSchema, userLoginSchema } from '../schemas/auth.schems.js';
-import {loginUser, registerStudent} from '../controllers/auth.controller.js'
+import { adminRegisterSchema, facultyRegisterSchema, studentRegisterSchema, userLoginSchema } from '../schemas/auth.schems.js';
+import {checkMe, loginUser, registerAdmin, registerFaculty, registerStudent} from '../controllers/auth.controller.js'
 import { asyncHandler } from '../utils/asyncHandler.js';
 import { authorizeMiddleware } from '../middlewares/authorizeMiddleware.js';
 import { authMiddleware } from '../middlewares/authMiddleware.js';
@@ -9,7 +9,13 @@ const authRouter = express.Router();
 
 
 authRouter.post('/login',validateMiddleware(userLoginSchema) ,asyncHandler(loginUser));
-authRouter.post('/register/student',authMiddleware,authorizeMiddleware("ADMIN") ,validateMiddleware(studentRegisterSchema), asyncHandler(registerStudent));
+authRouter.post('/register/student',validateMiddleware(studentRegisterSchema), authMiddleware,authorizeMiddleware("ADMIN"), asyncHandler(registerStudent));
+
+authRouter.post('/register/faculty', validateMiddleware(facultyRegisterSchema), authMiddleware, authorizeMiddleware("ADMIN"), asyncHandler(registerFaculty));
+
+authRouter.post('/register/admin', validateMiddleware(adminRegisterSchema), authMiddleware, authorizeMiddleware("ADMIN"), asyncHandler(registerAdmin));
+
+authRouter.get('/me', authMiddleware, checkMe);
 
 
 export default authRouter;
