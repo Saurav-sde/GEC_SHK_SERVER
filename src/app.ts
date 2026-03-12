a258658238a7bb949a9566b820804d1930f9be6f
Saurav Kumar <saurav.sde.cs@gmail.com>
2026-03-13 00:35:27 +0530

    added healthchekup route

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,18 @@
 import cookieParser from 'cookie-parser';
 import express from 'express';
-import authRouter from './routes/auth.router.js';
+import type { Request, Response } from 'express';
+import authRouter from './routes/auth.routes.js';
 import { globalErrorHandler } from './middlewares/error.middleware.js';
-import uploadRouter from './routes/upload.router.js';
+import uploadRouter from './routes/upload.routes.js';
+import swaggerUi from "swagger-ui-express";
+import { swaggerSpec } from './config/swagger.js';
 
 const app = express();
 // import cors from 'cors';
 
 app.use(express.json());
 app.use(cookieParser());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 
 // routes
@@ -20,5 +24,14 @@ app.use('/api/upload', uploadRouter)
 
 
 app.use(globalErrorHandler);
+
+
+app.get("/health", (req: Request,res: Response) => {
+    res.json({
+        status: "ok",
+        service: "GEC_SHK_SERVER"
+    });
+});
+
 export {app};
 
