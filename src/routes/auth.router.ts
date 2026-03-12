8a6fa51e33cc39f59e9c17af87c4e92a70bded81
Saurav Kumar <saurav.sde.cs@gmail.com>
2026-03-12 23:52:35 +0530

    fixed some import issues

--- src/routes/auth.router.ts
+++ src/routes/auth.router.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import { validateMiddleware } from '../middlewares/validateMiddleware.js';
-import { adminRegisterSchema, facultyRegisterSchema, studentRegisterSchema, userLoginSchema } from '../schemas/auth.schems.js';
+import { adminRegisterSchema, facultyRegisterSchema, studentRegisterSchema, userLoginSchema } from '../schemas/auth.schema.js';
 import {checkMe, loginUser, registerAdmin, registerFaculty, registerStudent} from '../controllers/auth.controller.js'
 import { asyncHandler } from '../utils/asyncHandler.js';
 import { authorizeMiddleware } from '../middlewares/authorizeMiddleware.js';
