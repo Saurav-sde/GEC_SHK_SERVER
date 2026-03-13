0630acd8de3769d5bb4dd46e86eec8d6388ffdbb
Saurav Kumar <saurav.sde.cs@gmail.com>
2026-03-14 00:36:40 +0530

    created routes for metaData

--- src/routes/metaData.routes.ts
+++ src/routes/metaData.routes.ts
@@ -3,10 +3,13 @@ import express from 'express';
 import { authMiddleware } from '../middlewares/authMiddleware.js';
 import { authorizeMiddleware } from '../middlewares/authorizeMiddleware.js';
 import { asyncHandler } from '../utils/asyncHandler.js';
-import { getStudentRegistrationMeta } from '../controllers/metaData.controller.js';
+import { getStudentFormData } from '../controllers/metaData.controller.js';
 
 
 
 const metaDataRouter = express.Router();
 
-metaDataRouter.get('/register-student', authMiddleware, authorizeMiddleware("ADMIN"), asyncHandler(getStudentRegistrationMeta));
+metaDataRouter.get('/register-student', authMiddleware, authorizeMiddleware("ADMIN"), asyncHandler(getStudentFormData));
+
+
+export default metaDataRouter;
