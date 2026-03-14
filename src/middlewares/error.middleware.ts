991e9c9ae0c881ef99d8f33ceb90036a716e8a94
Saurav Kumar <saurav.sde.cs@gmail.com>
2026-03-15 01:36:54 +0530

    updated error messages

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,5 +1,6 @@
 import type { NextFunction, Request, Response } from "express";
 import { ApiError } from "../utils/ApiError.js";
+import { success } from "zod";
 
 export const globalErrorHandler = (
   err: any,
@@ -7,17 +8,41 @@ export const globalErrorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  console.error("🔥 Error:", err);
 
-  // Prisma known errors
-  // UNIQUE constraint error
-  if (err.code === "P2002") {
-    const fields = (err.meta.driverAdapterError.cause.constraint.fields as string[])?.join(", ");
+  if(process.env.NODE_ENV !== "production") {
+    console.error("🔥 Error:", err);
+  }
+
+  // Prisma unique error
+  if(err.code === "P2002") {
+    const rawFields =
+      err.meta?.driverAdapterError?.cause?.constraint?.fields ??
+      err.meta?.target;
+
+    const fields = rawFields
+      ?.map((f: string) => f.replace(/"/g, ""))
+      .join(", ");
 
     return res.status(409).json({
       success: false,
-      message: `Duplicate value for ${fields}`,
-      errors: err.meta
+      message: `${fields || "Field"} already exists`,
+    });
+  }
+
+  // prisma record not found
+  if(err.code === "P2025") {
+    return res.json({
+      success: false,
+      message: "Record not found"
+    })
+  }
+
+  // foreign key errors
+  if(err.code === "P2003") {
+    const field = err.meta?.field_name?.split("_")[1] || "foreign key";
+    return res.status(400).json({
+      success: false,
+      message: `Invalid reference for ${field}`,
     });
   }
 
