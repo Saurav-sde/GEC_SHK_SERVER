24cef2fd0510378e3b99af51563384c6234bf9d8
Saurav Kumar <saurav.sde.cs@gmail.com>
2026-03-14 23:48:50 +0530

    added new routes for admin to get students details

--- src/routes/student.routes.ts
+++ src/routes/student.routes.ts
@@ -2,14 +2,14 @@ import express  from "express";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
 import { authorizeMiddleware } from "../middlewares/authorizeMiddleware.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-import { getStudent, getStudentById } from "../controllers/student.controller.js";
+import { getStudent, getStudentById, getStudentsForAdmin } from "../controllers/student.controller.js";
 
 
 
 const studentRouter = express.Router();
 
+studentRouter.get('/admin', authMiddleware, authorizeMiddleware("ADMIN") , asyncHandler(getStudentsForAdmin));
 studentRouter.get('/', authMiddleware, authorizeMiddleware("ADMIN"), asyncHandler(getStudent));
-
 studentRouter.get('/:id', authMiddleware, asyncHandler(getStudentById));
 
 export default studentRouter;
